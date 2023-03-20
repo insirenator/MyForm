89991d2af44be499f2b8ef018fea9a2bd88399f4
insirenator <arsshakeeb149@protonmail.com>
2023-03-20 10:38:31 +0530

    Added Local Storage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const submitBtn = document.querySelector('#submit-btn');
 const form = document.querySelector('#data-form');
+let userCount = 0;
 
 submitBtn.addEventListener('click', (e) => {
 	// prevent form submission
@@ -10,15 +11,27 @@ submitBtn.addEventListener('click', (e) => {
 	const values = [...formData.entries()];
 	console.log(values);
 
-	displayData(values);	
+	localStorage.setItem(userCount, JSON.stringify(values));
+
+	// Increment the user count
+	userCount++;
+
+
+	displayData();	
 });
 
-function displayData(values) {
+function displayData() {
 	const data_el = document.querySelector('.data');
 	data_el.innerHTML= '<h3 id="data-title">Details</h3>';
 	data_el.classList.remove('hide');
 
-	for (const value of values) {
+	for (let i = 0; i < userCount; i++) {
+		let values = JSON.parse(localStorage.getItem(i));
+
+		data_el.innerHTML += `<p class="user">USER ${i+1}</p>`;
+
+		for (const value of values) {
 		data_el.innerHTML += `<p><span class="entry">${value[0]} :</span> ${value[1]}</p>`; 
+	}
 	}
 }
